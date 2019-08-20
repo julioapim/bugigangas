7054b6d57c62f0aeb11a5dd50d9ac91b2841fdfa
Julio Pim <julio.pim@gruponzn.com>
2019-08-20 10:37:53 -0300

    html+css+js

---
+++ bootstrap/script.js
@@ -0,0 +1,43 @@
+/* js */
+var mensagem = "Soma";
+
+var lista = ["laranja", "maca"];
+
+var a = 1;
+var b = 9;
+var c = "Ola"
+var soma = a + b;
+
+console.log(mensagem + soma);
+
+/* function soma(var1,var2,var3){
+	if (b%2 == 1){
+		console.log("Numero impar");
+	}
+	else if (c%2 == 0){
+		console.log("Numero par")
+	}
+	else{
+		console.log("valor invalido")
+	}
+	return c;
+
+}
+var tentei = soma(10,10,"Ola")
+console.log("funcao na veia" + tentei);
+*/
+var i = 0;
+
+while (i < 3){
+	console.log(i);
+	i = i+1;
+}
+
+for (var j = 0 ; j < 3; j++){
+	console.log(j);
+}
+
+
+
+
+/**/
